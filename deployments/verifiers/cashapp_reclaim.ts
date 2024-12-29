086fb2299a04d24a4f4f1cb11a729601489e0461
Richard Liang <richardliang2015@u.northwestern.edu>
2024-12-29 11:41:36 -0500

    update cashapp provider hash generation

--- deployments/verifiers/cashapp_reclaim.ts
+++ deployments/verifiers/cashapp_reclaim.ts
@@ -1,9 +1,73 @@
 import { BigNumber } from "ethers";
 import { Currency } from "../../utils/protocolUtils";
+import { hashProviderParams } from "@zkp2p/reclaim-witness-sdk";
 
-export const CASHAPP_RECLAIM_PROVIDER_HASHES: any = [
-  "0x5e3b19d2559f94bb09ec06bb3b11a099f47af374d54c8b98ee21efa3054357b1",
-];
+export const getCashappReclaimProviderHashes = async (length: number) => {
+  const hashes = [];
+  for (let i = 0; i < length; i++) {
+    const hashed = hashProviderParams(
+      {
+        url: "https://cash.app/cash-app/activity/v1.0/page",
+        method: "POST",
+        responseMatches: [
+          {
+            "type": "regex",
+            "value": `"amount":(?<amount>[0-9]+)`,
+          },
+          {
+            "type": "regex",
+            "value": `"currency_code":"(?<currency_code>[^"]+)"`,
+          },
+          {
+            "type": "regex",
+            "value": `"display_date":(?<date>[0-9]+)`,
+          },
+          {
+            "type": "regex",
+            "value": `"cashtag":"(?<receiverId>[^"]+)"`,
+            "hash": true
+          },
+          {
+            "type": "regex",
+            "value": `"token":"(?<paymentId>[^"]+)"`,
+          },
+          {
+            "type": "regex",
+            "value": `"state":"(?<state>[^"]+)"`,
+          },
+        ],
+        responseRedactions: [
+          {
+            "jsonPath": `$.activity_rows[${i}].payment_history_inputs_row.payment.amount.amount`,
+            "xPath": ""
+          },
+          {
+            "jsonPath": `$.activity_rows[${i}].payment_history_inputs_row.payment.amount.currency_code`,
+            "xPath": ""
+          },
+          {
+            "jsonPath": `$.activity_rows[${i}].payment_history_inputs_row.payment.display_date`,
+            "xPath": ""
+          },
+          {
+            "jsonPath": `$.activity_rows[${i}].payment_history_inputs_row.recipient.cashtag`,
+            "xPath": ""
+          },
+          {
+            "jsonPath": `$.activity_rows[${i}].payment_history_inputs_row.payment.token`,
+            "xPath": ""
+          },
+          {
+            "jsonPath": `$.activity_rows[${i}].payment_history_inputs_row.payment.state`,
+            "xPath": ""
+          }
+        ],
+      }
+    );
+    hashes.push(hashed);
+  }
+  return hashes;
+};
 
 export const CASHAPP_RECLAIM_CURRENCIES: any = [
   Currency.USD,
