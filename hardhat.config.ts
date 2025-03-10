827b07de9376b4c61cd15485d3999eaccb753400
0xSachinK <0xsachink@gmail.com>
2025-03-11 00:04:14 +0530

    bump sepolia gas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -61,7 +61,7 @@ const config: HardhatUserConfig = {
       accounts: [
         `0x${process.env.TESTNET_DEPLOY_PRIVATE_KEY}`,
       ],
-      gasPrice: 50000000000, // 50 gwei
+      gasPrice: 300000000000, // 300 gwei
       gas: 8000000, // 8 million gas limit
       verify: {
         etherscan: {
