0c0426a90c69fb12323db001c7231d1381af6649
0xSachinK <0xsachink@gmail.com>
2025-05-17 01:24:41 +0530

    update boa and citi proofs

--- test/verifiers/zelleBoAReclaimVerifier.spec.ts
+++ test/verifiers/zelleBoAReclaimVerifier.spec.ts
@@ -22,21 +22,21 @@ const expect = getWaffleExpect();
 const zelleBoAExtensionProof = {
   "claim": {
     "provider": "http",
-    "parameters": "{\"body\":\"{\\\"filterV1\\\":{\\\"dateFilter\\\":{\\\"timeframeForHistory\\\":\\\"DEFAULTDAYS\\\"}},\\\"sortCriteriaV1\\\":{\\\"fieldName\\\":\\\"DATE\\\",\\\"order\\\":\\\"DESCENDING\\\"},\\\"pageInfo\\\":{\\\"pageNum\\\":1,\\\"pageSize\\\":\\\"\\\"}}\",\"headers\":{\"Accept\":\"application/json\",\"Accept-Language\":\"en-US\",\"Content-Type\":\"application/json\",\"Origin\":\"https://secure.bankofamerica.com\",\"Referer\":\"https://secure.bankofamerica.com/pay-transfer-pay-portal/?request_locale=en-us&returnSiteIndicator=GAIEC&target=paymentactivity\",\"Sec-Fetch-Dest\":\"empty\",\"Sec-Fetch-Mode\":\"cors\",\"Sec-Fetch-Site\":\"same-origin\",\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36\",\"X-Requested-With\":\"XMLHttpRequest\",\"sec-ch-ua\":\"\\\"Chromium\\\";v=\\\"136\\\", \\\"Google Chrome\\\";v=\\\"136\\\", \\\"Not.A/Brand\\\";v=\\\"99\\\"\",\"sec-ch-ua-mobile\":\"?0\",\"sec-ch-ua-platform\":\"\\\"macOS\\\"\"},\"method\":\"POST\",\"paramValues\":{},\"responseMatches\":[{\"type\":\"regex\",\"value\":\"\\\"confirmationNumber\\\":\\\"(?<confirmationNumber>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"status\\\":\\\"(?<status>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"transactionDate\\\":\\\"(?<transactionDate>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"amount\\\":(?<amount>[0-9\\\\.]+)\"},{\"hash\":true,\"type\":\"regex\",\"value\":\"\\\"aliasToken\\\":\\\"(?<aliasToken>[^\\\"]+)\\\"\"}],\"responseRedactions\":[{\"jsonPath\":\"$.completedTransactions[0].confirmationNumber\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].status\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].transactionDate\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].amount\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].targetAccount.aliasToken\",\"xPath\":\"\"}],\"url\":\"https://secure.bankofamerica.com/ogateway/payment-activity/api/v4/activity\"}",
+    "parameters": "{\"body\":\"{\\\"filterV1\\\":{\\\"dateFilter\\\":{\\\"timeframeForHistory\\\":\\\"DEFAULTDAYS\\\"}},\\\"sortCriteriaV1\\\":{\\\"fieldName\\\":\\\"DATE\\\",\\\"order\\\":\\\"DESCENDING\\\"},\\\"pageInfo\\\":{\\\"pageNum\\\":1,\\\"pageSize\\\":\\\"\\\"}}\",\"headers\":{\"Accept\":\"application/json\",\"Accept-Language\":\"en-US\",\"Content-Type\":\"application/json\",\"Origin\":\"https://secure.bankofamerica.com\",\"Referer\":\"https://secure.bankofamerica.com/pay-transfer-pay-portal/?request_locale=en-us&returnSiteIndicator=GAIMW&target=paymentactivity\",\"Sec-Fetch-Dest\":\"empty\",\"Sec-Fetch-Mode\":\"cors\",\"Sec-Fetch-Site\":\"same-origin\",\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36\",\"X-Requested-With\":\"XMLHttpRequest\",\"sec-ch-ua\":\"\\\"Chromium\\\";v=\\\"136\\\", \\\"Google Chrome\\\";v=\\\"136\\\", \\\"Not.A/Brand\\\";v=\\\"99\\\"\",\"sec-ch-ua-mobile\":\"?0\",\"sec-ch-ua-platform\":\"\\\"macOS\\\"\"},\"method\":\"POST\",\"paramValues\":{},\"responseMatches\":[{\"type\":\"regex\",\"value\":\"\\\"confirmationNumber\\\":\\\"(?<confirmationNumber>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"status\\\":\\\"(?<status>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"transactionDate\\\":\\\"(?<transactionDate>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"amount\\\":(?<amount>[0-9\\\\.]+)\"},{\"hash\":true,\"type\":\"regex\",\"value\":\"\\\"aliasToken\\\":\\\"(?<aliasToken>[^\\\"]+)\\\"\"}],\"responseRedactions\":[{\"jsonPath\":\"$.completedTransactions[0].confirmationNumber\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].status\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].transactionDate\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].amount\",\"xPath\":\"\"},{\"jsonPath\":\"$.completedTransactions[0].targetAccount.aliasToken\",\"xPath\":\"\"}],\"url\":\"https://secure.bankofamerica.com/ogateway/payment-activity/api/v4/activity\"}",
     "owner": "0xf9f25d1b846625674901ace47d6313d1ac795265",
-    "timestampS": 1746132575,
-    "context": "{\"contextAddress\":\"0x0\",\"contextMessage\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"extractedParameters\":{\"aliasToken\":\"0x3bcb39ffd57dd47e25c484c95ce7f7591305af0cfaaf7f18ab4ab548217fb303\",\"amount\":\"100.0\",\"confirmationNumber\":\"n5izkhusa\",\"status\":\"COMPLETED\",\"transactionDate\":\"2025-04-18\"},\"providerHash\":\"0x05eecaa422b995a513376f7ae0b3a16fab2bdcb7fb1eff38891475b56869a1bd\"}",
-    "identifier": "0x222e8d48a28ee89aa6b99766594e8a4daa6188c7a530ce8ec617e3d868863260",
+    "timestampS": 1747349461,
+    "context": "{\"contextAddress\":\"0x0\",\"contextMessage\":\"8326399457664203853385587893474801619762725624996440086480664263627804731444\",\"extractedParameters\":{\"aliasToken\":\"0x3bcb39ffd57dd47e25c484c95ce7f7591305af0cfaaf7f18ab4ab548217fb303\",\"amount\":\"5.0\",\"confirmationNumber\":\"osmgnjz2u\",\"status\":\"COMPLETED\",\"transactionDate\":\"2025-05-15\"},\"providerHash\":\"0x05eecaa422b995a513376f7ae0b3a16fab2bdcb7fb1eff38891475b56869a1bd\"}",
+    "identifier": "0x747ab3be259d0a33da0d6aeb2d8461454fa211f61ae6ffd1b8cbba54c015ad98",
     "epoch": 1
   },
   "signatures": {
     "attestorAddress": "0x0636c417755e3ae25c6c166d181c0607f4c572a3",
-    "claimSignature": { "0": 104, "1": 151, "2": 79, "3": 218, "4": 75, "5": 10, "6": 115, "7": 191, "8": 196, "9": 181, "10": 208, "11": 121, "12": 149, "13": 195, "14": 187, "15": 12, "16": 220, "17": 253, "18": 0, "19": 80, "20": 35, "21": 229, "22": 221, "23": 102, "24": 76, "25": 144, "26": 42, "27": 96, "28": 8, "29": 72, "30": 132, "31": 204, "32": 111, "33": 206, "34": 48, "35": 38, "36": 177, "37": 246, "38": 252, "39": 38, "40": 127, "41": 133, "42": 157, "43": 170, "44": 105, "45": 184, "46": 107, "47": 186, "48": 190, "49": 0, "50": 52, "51": 9, "52": 96, "53": 42, "54": 73, "55": 119, "56": 254, "57": 121, "58": 146, "59": 138, "60": 201, "61": 178, "62": 134, "63": 226, "64": 27 },
-    "resultSignature": { "0": 141, "1": 48, "2": 96, "3": 49, "4": 69, "5": 77, "6": 17, "7": 183, "8": 8, "9": 244, "10": 237, "11": 41, "12": 161, "13": 212, "14": 48, "15": 27, "16": 132, "17": 200, "18": 68, "19": 235, "20": 237, "21": 17, "22": 230, "23": 223, "24": 199, "25": 128, "26": 105, "27": 51, "28": 200, "29": 119, "30": 101, "31": 99, "32": 38, "33": 27, "34": 201, "35": 229, "36": 70, "37": 162, "38": 68, "39": 77, "40": 92, "41": 111, "42": 147, "43": 141, "44": 99, "45": 125, "46": 129, "47": 164, "48": 211, "49": 11, "50": 209, "51": 204, "52": 165, "53": 234, "54": 81, "55": 157, "56": 4, "57": 68, "58": 113, "59": 96, "60": 130, "61": 196, "62": 187, "63": 153, "64": 28 }
+    "claimSignature": { "0": 96, "1": 255, "2": 156, "3": 150, "4": 193, "5": 224, "6": 225, "7": 232, "8": 232, "9": 69, "10": 1, "11": 251, "12": 41, "13": 194, "14": 57, "15": 247, "16": 0, "17": 66, "18": 134, "19": 96, "20": 195, "21": 205, "22": 152, "23": 117, "24": 103, "25": 42, "26": 77, "27": 139, "28": 147, "29": 55, "30": 223, "31": 141, "32": 63, "33": 67, "34": 12, "35": 188, "36": 10, "37": 200, "38": 28, "39": 145, "40": 127, "41": 113, "42": 108, "43": 166, "44": 198, "45": 109, "46": 123, "47": 11, "48": 151, "49": 87, "50": 107, "51": 114, "52": 141, "53": 190, "54": 232, "55": 150, "56": 86, "57": 254, "58": 22, "59": 165, "60": 217, "61": 173, "62": 194, "63": 161, "64": 28 },
+    "resultSignature": { "0": 100, "1": 150, "2": 190, "3": 57, "4": 183, "5": 75, "6": 154, "7": 77, "8": 97, "9": 90, "10": 114, "11": 81, "12": 165, "13": 220, "14": 142, "15": 185, "16": 26, "17": 152, "18": 17, "19": 101, "20": 212, "21": 124, "22": 216, "23": 45, "24": 244, "25": 233, "26": 27, "27": 192, "28": 22, "29": 98, "30": 242, "31": 216, "32": 43, "33": 139, "34": 211, "35": 210, "36": 142, "37": 154, "38": 140, "39": 132, "40": 25, "41": 252, "42": 130, "43": 29, "44": 223, "45": 97, "46": 58, "47": 58, "48": 91, "49": 15, "50": 238, "51": 148, "52": 155, "53": 254, "54": 54, "55": 137, "56": 203, "57": 83, "58": 66, "59": 37, "60": 249, "61": 183, "62": 196, "63": 235, "64": 27 }
   }
 };
 
-describe("ZelleBoAReclaimVerifier", () => {
+describe.only("ZelleBoAReclaimVerifier", () => {
   let owner: Account;
   let attacker: Account;
   let baseVerifier: Account;
@@ -104,15 +104,15 @@ describe("ZelleBoAReclaimVerifier", () => {
       proof = parseExtensionProof(zelleBoAExtensionProof);
       subjectProof = encodeProof(proof);
 
-      const paymentTimeString = '2025-04-18';
+      const paymentTimeString = '2025-05-15';
       const paymentTime = new Date(paymentTimeString);
       paymentTimestamp = Math.ceil(paymentTime.getTime() / 1000);
 
       subjectCaller = baseVerifier;
       subjectDepositToken = usdcToken.address;
-      subjectIntentAmount = usdc(110);
+      subjectIntentAmount = usdc(5);
       subjectIntentTimestamp = BigNumber.from(paymentTimestamp);
-      subjectConversionRate = ether(0.9);   // 110 * 0.9 = 99 [intent amount * conversion rate = payment amount]
+      subjectConversionRate = ether(0.99);   // 5 * 0.99 = 4.95 [intent amount * conversion rate = payment amount]
       subjectPayeeDetailsHash = "0x3bcb39ffd57dd47e25c484c95ce7f7591305af0cfaaf7f18ab4ab548217fb303";
       subjectFiatCurrency = ZERO_BYTES32;
       subjectData = ethers.utils.defaultAbiCoder.encode(
@@ -154,13 +154,13 @@ describe("ZelleBoAReclaimVerifier", () => {
       ] = await subjectCallStatic();
 
       expect(verified).to.be.true;
-      expect(intentHash).to.eq("0x0000000000000000000000000000000000000000000000000000000000000000");
+      expect(intentHash).to.eq(BigNumber.from('8326399457664203853385587893474801619762725624996440086480664263627804731444').toHexString());
     });
 
     it("should nullify the payment id", async () => {
       await subject();
 
-      const nullifier = ethers.utils.keccak256(ethers.utils.solidityPack(['string'], ['n5izkhusa']));
+      const nullifier = ethers.utils.keccak256(ethers.utils.solidityPack(['string'], ['osmgnjz2u']));
       const isNullified = await nullifierRegistry.isNullified(nullifier);
 
       expect(isNullified).to.be.true;
@@ -179,7 +179,7 @@ describe("ZelleBoAReclaimVerifier", () => {
 
     describe("when the payment amount is less than the intent amount", async () => {
       beforeEach(async () => {
-        subjectIntentAmount = usdc(1000);  // 1000 * 0.9 = 900 [900 > 100]
+        subjectIntentAmount = usdc(5.1);  // 5.1 * 0.99 = 5.049 [5.049 < 5]
       });
 
       it("should revert", async () => {
--- test/verifiers/zelleCitiReclaimVerifier.spec.ts
+++ test/verifiers/zelleCitiReclaimVerifier.spec.ts
@@ -20,35 +20,155 @@ import {
 const expect = getWaffleExpect();
 
 const zelleCitiExtensionProof = {
-  claim: {
-    provider: "http",
-    parameters: "{\"body\":\"\",\"headers\":{\"Accept\":\"application/json\",\"Accept-language\":\"en_US\",\"Content-Type\":\"application/json\",\"Referer\":\"https://online.citi.com/US/nga/zelle/transfer\",\"Sec-Fetch-Dest\":\"empty\",\"Sec-Fetch-Mode\":\"cors\",\"Sec-Fetch-Site\":\"same-origin\",\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36\",\"appVersion\":\"CBOL-ANG-2025-04-01\",\"businessCode\":\"GCB\",\"channelId\":\"CBOL\",\"client_id\":\"4a51fb19-a1a7-4247-bc7e-18aa56dd1c40\",\"countryCode\":\"US\",\"sec-ch-ua\":\"\\\"Chromium\\\";v=\\\"136\\\", \\\"Google Chrome\\\";v=\\\"136\\\", \\\"Not.A/Brand\\\";v=\\\"99\\\"\",\"sec-ch-ua-mobile\":\"?0\",\"sec-ch-ua-platform\":\"\\\"macOS\\\"\"},\"method\":\"GET\",\"paramValues\":{},\"responseMatches\":[{\"type\":\"regex\",\"value\":\"\\\"paymentID\\\":\\\"(?<paymentID>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"paymentStatus\\\":\\\"(?<paymentStatus>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"updatedTimeStamp\\\":\\\"(?<updatedTimeStamp>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"amount\\\":\\\"(?<amount>[^\\\"]+)\\\"\"},{\"hash\":true,\"type\":\"regex\",\"value\":\"\\\"partyToken\\\":\\\"(?<partyToken>[^\\\"]+)\\\"\"}],\"responseRedactions\":[{\"jsonPath\":\"$.content.paymentTransactionsData[1].paymentID\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[1].paymentStatus\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[1].updatedTimeStamp\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[1].amount\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[1].partyToken\",\"xPath\":\"\"}],\"url\":\"https://online.citi.com/gcgapi/prod/public/v1/p2ppayments/pastActivityTransactions?transactionCount=20&pageId=0&tab=All\"}",
-    owner: "0xf9f25d1b846625674901ace47d6313d1ac795265",
-    timestampS: 1746152327,
-    context: "{\"contextAddress\":\"0x0\",\"contextMessage\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"extractedParameters\":{\"amount\":\"10.00\",\"partyToken\":\"0x3bcb39ffd57dd47e25c484c95ce7f7591305af0cfaaf7f18ab4ab548217fb303\",\"paymentID\":\"CTIwjcKauxso\",\"paymentStatus\":\"DELIVERED\",\"updatedTimeStamp\":\"04/28/2025\"},\"providerHash\":\"0x3a3d23f3f3c5063af7fa0bd48aa98f4cd4658770aa5de971924fda7ef92ea743\"}",
-    identifier: "0x3694f804d9aabaab9a31bda50e6a1491c73d1067d9bec558ba0dba6e34d8c0c1",
-    epoch: 1
+  "claim": {
+    "provider": "http",
+    "parameters": "{\"body\":\"\",\"headers\":{\"Accept\":\"application/json\",\"Accept-language\":\"en_US\",\"Content-Type\":\"application/json\",\"Referer\":\"https://online.citi.com/US/nga/zelle/transfer\",\"Sec-Fetch-Dest\":\"empty\",\"Sec-Fetch-Mode\":\"cors\",\"Sec-Fetch-Site\":\"same-origin\",\"User-Agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36\",\"appVersion\":\"CBOL-ANG-2025-04-01\",\"businessCode\":\"GCB\",\"channelId\":\"CBOL\",\"client_id\":\"4a51fb19-a1a7-4247-bc7e-18aa56dd1c40\",\"countryCode\":\"US\",\"sec-ch-ua\":\"\\\"Chromium\\\";v=\\\"136\\\", \\\"Google Chrome\\\";v=\\\"136\\\", \\\"Not.A/Brand\\\";v=\\\"99\\\"\",\"sec-ch-ua-mobile\":\"?0\",\"sec-ch-ua-platform\":\"\\\"macOS\\\"\"},\"method\":\"GET\",\"paramValues\":{},\"responseMatches\":[{\"type\":\"regex\",\"value\":\"\\\"paymentID\\\":\\\"(?<paymentID>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"paymentStatus\\\":\\\"(?<paymentStatus>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"updatedTimeStamp\\\":\\\"(?<updatedTimeStamp>[^\\\"]+)\\\"\"},{\"type\":\"regex\",\"value\":\"\\\"amount\\\":\\\"(?<amount>[^\\\"]+)\\\"\"},{\"hash\":true,\"type\":\"regex\",\"value\":\"\\\"partyToken\\\":\\\"(?<partyToken>[^\\\"]+)\\\"\"}],\"responseRedactions\":[{\"jsonPath\":\"$.content.paymentTransactionsData[2].paymentID\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[2].paymentStatus\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[2].updatedTimeStamp\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[2].amount\",\"xPath\":\"\"},{\"jsonPath\":\"$.content.paymentTransactionsData[2].partyToken\",\"xPath\":\"\"}],\"url\":\"https://online.citi.com/gcgapi/prod/public/v1/p2ppayments/pastActivityTransactions?transactionCount=20&pageId=0&tab=All\"}",
+    "owner": "0xf9f25d1b846625674901ace47d6313d1ac795265",
+    "timestampS": 1747334657,
+    "context": "{\"contextAddress\":\"0x0\",\"contextMessage\":\"4948915460758196888156147053328476497446483899021706653248173960948416723660\",\"extractedParameters\":{\"amount\":\"10.00\",\"partyToken\":\"0x3bcb39ffd57dd47e25c484c95ce7f7591305af0cfaaf7f18ab4ab548217fb303\",\"paymentID\":\"CTIwjcKauxso\",\"paymentStatus\":\"DELIVERED\",\"updatedTimeStamp\":\"04/28/2025\"},\"providerHash\":\"0x2a20d5d1fc3ccfa7f3053949fb067cb56447eb46cb415a10a496c36c5f9992d7\"}",
+    "identifier": "0x6eee8481892033bb43385634044cc0fb8b8e970790ad9f53d750996447c42064",
+    "epoch": 1
   },
-  signatures: {
-    attestorAddress: "0x0636c417755e3ae25c6c166d181c0607f4c572a3",
-    claimSignature: [
-      57, 148, 209, 226, 97, 236, 95, 190, 18, 226, 221, 93, 3, 198, 5, 37,
-      26, 240, 232, 94, 239, 88, 167, 95, 203, 144, 57, 45, 44, 173, 113, 154,
-      69, 45, 240, 5, 89, 231, 124, 226, 229, 145, 71, 212, 12, 145, 18, 172,
-      42, 149, 206, 165, 180, 103, 20, 82, 38, 216, 48, 146, 3, 102, 72, 166,
-      27
-    ],
-    resultSignature: [
-      17, 242, 170, 113, 221, 222, 14, 161, 137, 83, 210, 215, 33, 132, 6, 35,
-      250, 214, 225, 115, 179, 43, 192, 116, 223, 228, 166, 6, 157, 78, 39, 99,
-      57, 100, 221, 158, 121, 58, 154, 173, 110, 92, 207, 109, 123, 233, 215, 49,
-      34, 68, 116, 80, 220, 180, 205, 57, 217, 218, 218, 112, 48, 145, 251, 30,
-      28
-    ]
+  "signatures": {
+    "attestorAddress": "0x0636c417755e3ae25c6c166d181c0607f4c572a3",
+    "claimSignature": {
+      "0": 82,
+      "1": 107,
+      "2": 128,
+      "3": 95,
+      "4": 29,
+      "5": 193,
+      "6": 249,
+      "7": 14,
+      "8": 77,
+      "9": 250,
+      "10": 4,
+      "11": 237,
+      "12": 167,
+      "13": 126,
+      "14": 238,
+      "15": 152,
+      "16": 96,
+      "17": 38,
+      "18": 111,
+      "19": 248,
+      "20": 114,
+      "21": 187,
+      "22": 107,
+      "23": 146,
+      "24": 5,
+      "25": 150,
+      "26": 153,
+      "27": 197,
+      "28": 181,
+      "29": 96,
+      "30": 121,
+      "31": 42,
+      "32": 115,
+      "33": 53,
+      "34": 169,
+      "35": 203,
+      "36": 246,
+      "37": 231,
+      "38": 59,
+      "39": 133,
+      "40": 23,
+      "41": 46,
+      "42": 121,
+      "43": 12,
+      "44": 80,
+      "45": 49,
+      "46": 28,
+      "47": 83,
+      "48": 106,
+      "49": 230,
+      "50": 243,
+      "51": 155,
+      "52": 77,
+      "53": 177,
+      "54": 244,
+      "55": 194,
+      "56": 231,
+      "57": 52,
+      "58": 95,
+      "59": 128,
+      "60": 246,
+      "61": 180,
+      "62": 40,
+      "63": 74,
+      "64": 28
+    },
+    "resultSignature": {
+      "0": 188,
+      "1": 182,
+      "2": 94,
+      "3": 56,
+      "4": 117,
+      "5": 176,
+      "6": 177,
+      "7": 79,
+      "8": 112,
+      "9": 41,
+      "10": 176,
+      "11": 78,
+      "12": 85,
+      "13": 148,
+      "14": 153,
+      "15": 82,
+      "16": 156,
+      "17": 32,
+      "18": 160,
+      "19": 111,
+      "20": 195,
+      "21": 91,
+      "22": 158,
+      "23": 193,
+      "24": 223,
+      "25": 211,
+      "26": 248,
+      "27": 37,
+      "28": 2,
+      "29": 111,
+      "30": 245,
+      "31": 11,
+      "32": 126,
+      "33": 179,
+      "34": 166,
+      "35": 127,
+      "36": 123,
+      "37": 103,
+      "38": 117,
+      "39": 85,
+      "40": 185,
+      "41": 119,
+      "42": 202,
+      "43": 75,
+      "44": 213,
+      "45": 180,
+      "46": 189,
+      "47": 55,
+      "48": 163,
+      "49": 98,
+      "50": 152,
+      "51": 242,
+      "52": 232,
+      "53": 64,
+      "54": 174,
+      "55": 239,
+      "56": 110,
+      "57": 233,
+      "58": 149,
+      "59": 204,
+      "60": 218,
+      "61": 1,
+      "62": 82,
+      "63": 91,
+      "64": 27
+    }
   }
-};
+}
 
-describe("ZelleCitiReclaimVerifier", () => {
+describe.only("ZelleCitiReclaimVerifier", () => {
   let owner: Account;
   let attacker: Account;
   let baseVerifier: Account;
@@ -72,7 +192,7 @@ describe("ZelleCitiReclaimVerifier", () => {
     usdcToken = await deployer.deployUSDCMock(usdc(1000000000), "USDC", "USDC");
 
     witnesses = ["0x0636c417755e3ae25c6c166d181c0607f4c572a3"];
-    providerHashes = ["0x3a3d23f3f3c5063af7fa0bd48aa98f4cd4658770aa5de971924fda7ef92ea743"];
+    providerHashes = ["0x2a20d5d1fc3ccfa7f3053949fb067cb56447eb46cb415a10a496c36c5f9992d7"];
 
     nullifierRegistry = await deployer.deployNullifierRegistry();
 
@@ -165,7 +285,7 @@ describe("ZelleCitiReclaimVerifier", () => {
       ] = await subjectCallStatic();
 
       expect(verified).to.be.true;
-      expect(intentHash).to.eq("0x0000000000000000000000000000000000000000000000000000000000000000");
+      expect(intentHash).to.eq(BigNumber.from('4948915460758196888156147053328476497446483899021706653248173960948416723660').toHexString());
     });
 
     it("should nullify the payment id", async () => {
@@ -231,7 +351,7 @@ describe("ZelleCitiReclaimVerifier", () => {
     describe("when the provider hash is invalid", async () => {
       beforeEach(async () => {
         proof.claimInfo.context = proof.claimInfo.context.replace(
-          "0x3a3d23f3f3c5063af7fa0bd48aa98f4cd4658770aa5de971924fda7ef92ea743",
+          "0x2a20d5d1fc3ccfa7f3053949fb067cb56447eb46cb415a10a496c36c5f9992d7",
           "0xbbb4d6813c1ccac7253673094ce4c1e122fe358682392851cfa332fe8359b8fc"
         );
         proof.signedClaim.claim.identifier = getIdentifierFromClaimInfo(proof.claimInfo);
