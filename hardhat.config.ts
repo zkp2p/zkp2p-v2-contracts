5df69f8bab1dc71d499d25db02442932cf1fb54d
Sachin <0xsachink@gmail.com>
2025-09-23 18:21:15 +0800

    chore: bump etherscan api to v2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -116,7 +116,7 @@ const config: HardhatUserConfig = {
         network: "baseSepolia",
         chainId: 84532,
         urls: {
-          apiURL: "https://api-sepolia.basescan.org/api",
+          apiURL: "https://api.etherscan.io/v2/api?chainid=84532",
           browserURL: "https://sepolia.basescan.org"
         }
       },
@@ -124,7 +124,7 @@ const config: HardhatUserConfig = {
         network: "base",
         chainId: 8453,
         urls: {
-          apiURL: "https://api.basescan.org/api",
+          apiURL: "https://api.etherscan.io/v2/api?chainid=8453",
           browserURL: "https://basescan.org"
         }
       }
